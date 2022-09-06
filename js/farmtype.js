8c7eb50731965ae73b245ae81c905be93634bd58
Chris Robles <chris.robles@ppfa.org>
2022-09-06 09:38:21 -0500

    ClientVersion: 42

---
+++ js/farmtype.js
@@ -0,0 +1,24 @@
+// source: ei.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.ei.FarmType');
+
+/**
+ * @enum {number}
+ */
+proto.ei.FarmType = {
+  EMPTY: 1,
+  HOME: 2,
+  CONTRACT: 3
+};
+
---
+++ js/goaltype.js
@@ -0,0 +1,23 @@
+// source: ei.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.ei.GoalType');
+
+/**
+ * @enum {number}
+ */
+proto.ei.GoalType = {
+  EGGS_LAID: 1,
+  UNKNOWN_GOAL: 100
+};
+
